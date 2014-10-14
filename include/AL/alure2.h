865d7363ca3f60db1b279a734ebd9ce1107a6eaa
Chris Robinson <chris.kcat@gmail.com>
2014-10-13 23:21:55 -0700

    Reorder some class definitions

--- include/AL/alure2.h
+++ include/AL/alure2.h
@@ -14,39 +14,6 @@ class Context;
 class DeviceManager;
 
 
-class Context {
-protected:
-    virtual ~Context() { }
-
-public:
-    static bool MakeCurrent(Context *context);
-    static Context *GetCurrent();
-
-    virtual void destroy() = 0;
-
-    virtual Device *getDevice() = 0;
-};
-
-
-enum PlaybackDeviceType {
-    PlaybackDevType_Basic = ALC_DEVICE_SPECIFIER,
-    PlaybackDevType_Complete = ALC_ALL_DEVICES_SPECIFIER
-};
-
-class Device {
-protected:
-    virtual ~Device() { }
-
-public:
-    virtual std::string getName(PlaybackDeviceType type) = 0;
-    virtual bool queryExtension(const char *extname) = 0;
-
-    virtual Context *createContext(ALCint *attribs=0) = 0;
-
-    virtual void close() = 0;
-};
-
-
 enum DeviceEnumeration {
     DevEnum_Basic = ALC_DEVICE_SPECIFIER,
     DevEnum_Complete = ALC_ALL_DEVICES_SPECIFIER,
@@ -72,6 +39,39 @@ public:
     virtual std::string defaultDeviceName(DefaultDeviceType type) = 0;
 
     virtual Device *openPlayback(const std::string &name=std::string()) = 0;
+};
+
+
+enum PlaybackDeviceType {
+    PlaybackDevType_Basic = ALC_DEVICE_SPECIFIER,
+    PlaybackDevType_Complete = ALC_ALL_DEVICES_SPECIFIER
+};
+
+class Device {
+protected:
+    virtual ~Device() { }
+
+public:
+    virtual std::string getName(PlaybackDeviceType type) = 0;
+    virtual bool queryExtension(const char *extname) = 0;
+
+    virtual Context *createContext(ALCint *attribs=0) = 0;
+
+    virtual void close() = 0;
+};
+
+
+class Context {
+protected:
+    virtual ~Context() { }
+
+public:
+    static bool MakeCurrent(Context *context);
+    static Context *GetCurrent();
+
+    virtual void destroy() = 0;
+
+    virtual Device *getDevice() = 0;
 };
 
 } // namespace alure
