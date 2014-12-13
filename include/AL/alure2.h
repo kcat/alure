7bcc02fed7c2206ae2ec62510292bb65b7d6ed93
Chris Robinson <chris.kcat@gmail.com>
2014-12-13 03:15:11 -0800

    Move Vector3 to alure2.h

--- include/AL/alure2.h
+++ include/AL/alure2.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include <string>
 #include <memory>
+#include <cmath>
 
 #include "alc.h"
 #include "al.h"
@@ -56,6 +57,88 @@ struct FilterParams {
     ALfloat mGainHF; // For low-pass and band-pass filters
     ALfloat mGainLF; // For high-pass and band-pass filters
 };
+
+
+class Vector3 {
+    ALfloat mValue[3];
+
+public:
+    Vector3() { }
+    Vector3(const Vector3 &rhs) : mValue{rhs.mValue[0], rhs.mValue[1], rhs.mValue[2]}
+    { }
+    Vector3(ALfloat val) : mValue{val, val, val}
+    { }
+    Vector3(ALfloat x, ALfloat y, ALfloat z) : mValue{x, y, z}
+    { }
+    Vector3(const ALfloat *vec) : mValue{vec[0], vec[1], vec[2]}
+    { }
+
+    const ALfloat *getPtr() const
+    { return mValue; }
+
+    ALfloat& operator[](size_t i)
+    { return mValue[i]; }
+    const ALfloat& operator[](size_t i) const
+    { return mValue[i]; }
+
+#define ALURE_DECL_OP(op)                           \
+    Vector3 operator op(const Vector3 &rhs) const   \
+    {                                               \
+        return Vector3(mValue[0] op rhs.mValue[0],  \
+                       mValue[1] op rhs.mValue[1],  \
+                       mValue[2] op rhs.mValue[2]); \
+    }
+    ALURE_DECL_OP(+)
+    ALURE_DECL_OP(-)
+    ALURE_DECL_OP(*)
+    ALURE_DECL_OP(/)
+#undef ALURE_DECL_OP
+#define ALURE_DECL_OP(op)                    \
+    Vector3& operator op(const Vector3 &rhs) \
+    {                                        \
+        mValue[0] op rhs.mValue[0];          \
+        mValue[1] op rhs.mValue[1];          \
+        mValue[2] op rhs.mValue[2];          \
+        return *this;                        \
+    }
+    ALURE_DECL_OP(+=)
+    ALURE_DECL_OP(-=)
+    ALURE_DECL_OP(*=)
+    ALURE_DECL_OP(/=)
+#undef ALURE_DECL_OP
+#define ALURE_DECL_OP(op)                    \
+    Vector3 operator op(ALfloat scale) const \
+    {                                        \
+        return Vector3(mValue[0] op scale,   \
+                       mValue[1] op scale,   \
+                       mValue[2] op scale);  \
+    }
+    ALURE_DECL_OP(*)
+    ALURE_DECL_OP(/)
+#undef ALURE_DECL_OP
+#define ALURE_DECL_OP(op)               \
+    Vector3& operator op(ALfloat scale) \
+    {                                   \
+        mValue[0] op scale;             \
+        mValue[1] op scale;             \
+        mValue[2] op scale;             \
+        return *this;                   \
+    }
+    ALURE_DECL_OP(*=)
+    ALURE_DECL_OP(/=)
+#undef ALURE_DECL_OP
+
+    ALfloat getLengthSquared() const
+    { return mValue[0]*mValue[0] + mValue[1]*mValue[1] + mValue[2]*mValue[2]; }
+    ALfloat getLength() const
+    { return sqrtf(getLengthSquared()); }
+
+    ALfloat getDistanceSquared(const Vector3 &pos) const
+    { return (pos - *this).getLengthSquared(); }
+    ALfloat getDistance(const Vector3 &pos) const
+    { return (pos - *this).getLength(); }
+};
+static_assert(sizeof(Vector3) == sizeof(ALfloat[3]), "Bad Vector3 size");
 
 
 /**
--- src/main.h
+++ src/main.h
@@ -3,8 +3,6 @@
 
 #include "alure2.h"
 
-#include <cmath>
-
 namespace alure
 {
 
@@ -15,87 +13,6 @@ inline T cast(T2 obj)
 #else
 { return obj ? dynamic_cast<T>(obj) : 0; }
 #endif
-
-class Vector3 {
-    ALfloat mValue[3];
-
-public:
-    Vector3() { }
-    Vector3(const Vector3 &rhs) : mValue{rhs.mValue[0], rhs.mValue[1], rhs.mValue[2]}
-    { }
-    Vector3(ALfloat val) : mValue{val, val, val}
-    { }
-    Vector3(ALfloat x, ALfloat y, ALfloat z) : mValue{x, y, z}
-    { }
-    Vector3(const ALfloat *vec) : mValue{vec[0], vec[1], vec[2]}
-    { }
-
-    const ALfloat *getPtr() const
-    { return mValue; }
-
-    ALfloat& operator[](size_t i)
-    { return mValue[i]; }
-    const ALfloat& operator[](size_t i) const
-    { return mValue[i]; }
-
-#define ALURE_DECL_OP(op)                           \
-    Vector3 operator op(const Vector3 &rhs) const   \
-    {                                               \
-        return Vector3(mValue[0] op rhs.mValue[0],  \
-                       mValue[1] op rhs.mValue[1],  \
-                       mValue[2] op rhs.mValue[2]); \
-    }
-    ALURE_DECL_OP(+)
-    ALURE_DECL_OP(-)
-    ALURE_DECL_OP(*)
-    ALURE_DECL_OP(/)
-#undef ALURE_DECL_OP
-#define ALURE_DECL_OP(op)                    \
-    Vector3& operator op(const Vector3 &rhs) \
-    {                                        \
-        mValue[0] op rhs.mValue[0];          \
-        mValue[1] op rhs.mValue[1];          \
-        mValue[2] op rhs.mValue[2];          \
-        return *this;                        \
-    }
-    ALURE_DECL_OP(+=)
-    ALURE_DECL_OP(-=)
-    ALURE_DECL_OP(*=)
-    ALURE_DECL_OP(/=)
-#undef ALURE_DECL_OP
-#define ALURE_DECL_OP(op)                    \
-    Vector3 operator op(ALfloat scale) const \
-    {                                        \
-        return Vector3(mValue[0] op scale,   \
-                       mValue[1] op scale,   \
-                       mValue[2] op scale);  \
-    }
-    ALURE_DECL_OP(*)
-    ALURE_DECL_OP(/)
-#undef ALURE_DECL_OP
-#define ALURE_DECL_OP(op)               \
-    Vector3& operator op(ALfloat scale) \
-    {                                   \
-        mValue[0] op scale;             \
-        mValue[1] op scale;             \
-        mValue[2] op scale;             \
-        return *this;                   \
-    }
-    ALURE_DECL_OP(*=)
-    ALURE_DECL_OP(/=)
-#undef ALURE_DECL_OP
-
-    ALfloat getLengthSquared() const
-    { return mValue[0]*mValue[0] + mValue[1]*mValue[1] + mValue[2]*mValue[2]; }
-    ALfloat getLength() const
-    { return sqrtf(getLengthSquared()); }
-
-    ALfloat getDistanceSquared(const Vector3 &pos) const
-    { return (pos - *this).getLengthSquared(); }
-    ALfloat getDistance(const Vector3 &pos) const
-    { return (pos - *this).getLength(); }
-};
-static_assert(sizeof(Vector3) == sizeof(ALfloat[3]), "Bad Vector3 size");
 
 } // namespace alure
 
