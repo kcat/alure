ecb0f777882756f525a6eba17a10f1033d1ef0bb
Chris Robinson <chris.kcat@gmail.com>
2016-11-19 15:12:49 -0800

    Allow alure-enum to get info for a specific device

--- examples/alure-enum.cpp
+++ examples/alure-enum.cpp
@@ -3,7 +3,7 @@
 
 #include "alure2.h"
 
-int main()
+int main(int argc, char *argv[])
 {
     alure::DeviceManager &devMgr = alure::DeviceManager::get();
     alure::Vector<alure::String> list;
@@ -11,34 +11,42 @@ int main()
 
     list = devMgr.enumerate(alure::DevEnum_Basic);
     defname = devMgr.defaultDeviceName(alure::DefaultDevType_Basic);
-    std::cout<< "Available basic devices:" <<std::endl;
+    std::cout<< "Available basic devices:\n";
     for(const auto &name : list)
-        std::cout<< "  "<<name<<((defname==name)?"  [DEFAULT]":"") <<std::endl;
+        std::cout<< "  "<<name<<((defname==name)?"  [DEFAULT]":"") <<'\n';
     std::cout<<std::endl;
 
     list = devMgr.enumerate(alure::DevEnum_Complete);
     defname = devMgr.defaultDeviceName(alure::DefaultDevType_Complete);
-    std::cout<< "Available devices:" <<std::endl;
+    std::cout<< "Available devices:\n";
     for(const auto &name : list)
-        std::cout<< "  "<<name<<((defname==name)?"  [DEFAULT]":"") <<std::endl;
+        std::cout<< "  "<<name<<((defname==name)?"  [DEFAULT]":"") <<'\n';
     std::cout<<std::endl;
 
     list = devMgr.enumerate(alure::DevEnum_Capture);
     defname = devMgr.defaultDeviceName(alure::DefaultDevType_Capture);
-    std::cout<< "Available capture devices:" <<std::endl;
+    std::cout<< "Available capture devices:\n";
     for(const auto &name : list)
-        std::cout<< "  "<<name<<((defname==name)?"  [DEFAULT]":"") <<std::endl;
+        std::cout<< "  "<<name<<((defname==name)?"  [DEFAULT]":"") <<'\n';
     std::cout<<std::endl;
 
-    ALCuint version;
-    alure::Device *dev = devMgr.openPlayback();
+    alure::Device *dev = [&devMgr, argc, argv]()
+    {
+        if(argc > 1) try {
+            return devMgr.openPlayback(argv[1]);
+        }
+        catch(std::exception &e) {
+            std::cerr<< "Exception while opening requested device: "<<e.what() <<std::endl;
+        }
+        return devMgr.openPlayback();
+    }();
     std::cout<< "Info for device \""<<dev->getName(alure::PlaybackDeviceName_Complete)<<"\":" <<std::endl;
-    version = dev->getALCVersion();
+    ALCuint version = dev->getALCVersion();
     std::cout<< "ALC version: "<<alure::MajorVersion(version)<<"."<<alure::MinorVersion(version) <<std::endl;
     version = dev->getEFXVersion();
     if(version)
     {
-        std::cout<< "EFX version: "<<alure::MajorVersion(version)<<"."<<alure::MinorVersion(version) <<std::endl;
+        std::cout<< "EFX version: "<<alure::MajorVersion(version)<<"."<<alure::MinorVersion(version) <<'\n';
         std::cout<< "Max auxiliary sends: "<<dev->getMaxAuxiliarySends() <<std::endl;
     }
     else
