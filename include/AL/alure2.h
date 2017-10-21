eae91cca905cecd0676ce216c05e99c241825578
Chris Robinson <chris.kcat@gmail.com>
2017-10-21 14:31:03 -0700

    Add methods to convert between dB and linear gain

--- include/AL/alure2.h
+++ include/AL/alure2.h
@@ -316,6 +316,18 @@ inline std::basic_ostream<T>& operator<<(std::basic_ostream<T,Tr> &lhs, BasicStr
 }
 
 
+/** Convert a value from decibels to linear gain.  */
+template<typename T, typename NonRefT=RemoveRefT<T>,
+         typename=EnableIfT<std::is_floating_point<NonRefT>::value>>
+constexpr inline NonRefT dBToLinear(T&& value)
+{ return std::pow(NonRefT(10.0), std::forward<T&&>(value) / NonRefT(20.0)); }
+
+/** Convert a value from linear gain to decibels.  */
+template<typename T, typename NonRefT=RemoveRefT<T>,
+         typename=EnableIfT<std::is_floating_point<NonRefT>::value>>
+constexpr inline NonRefT LinearTodB(T&& value)
+{ return std::log10(std::forward<T&&>(value)) * NonRefT(20.0); }
+
 /**
  * An attribute pair, for passing attributes to Device::createContext and
  * Device::reset.
